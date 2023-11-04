0a0c2986933608895d6a2d2a026ac3af60bf1414
NORTH89 <nordintata0@gmail.com>
2023-11-04 18:05:14 +0100

    create first file shell.h

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#endif
