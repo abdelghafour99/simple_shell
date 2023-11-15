99c4b472025efff23d1f69a15bbbc06bf9751ff8
NORTH89 <nordintata0@gmail.com>
2023-11-15 13:07:17 +0100

    update my code

--- exec_alias.c
+++ exec_alias.c
@@ -35,13 +35,41 @@ void exec_alias(char **args)
 	int alias_count = 0, i = 1;
 
 	insert_existing_aliases(alias_count, aliases);
-
 	if (_strcmp(args[0], "alias") == 0)
 	{
 		if (args[1] == NULL)
 			display_aliases(alias_count, aliases);
 		else
-			handle_alias_cmds(args, &i, alias_count, aliases);
+		{
+			while (args[i] != NULL)
+			{
+				const char *arg = args[i];
+				const char *equals_sign = strchr(arg, '=');
+
+				if (equals_sign)
+				{
+					char alias_name[MAX_ALIAS_NAME_LENGTH];
+					char alias_value[MAX_ALIAS_VALUE_LENGTH];
+
+					strncpy(alias_name, arg, equals_sign - arg);
+					alias_name[equals_sign - arg] = '\0';
+					strcpy(alias_value, equals_sign + 1);
+					if (add_alias(alias_name, alias_value, alias_count, aliases) == 0)
+						return;
+				}
+				else
+				{
+					const char *alias_name = args[i];
+					const char *alias_value = get_alias(alias_name, alias_count, aliases);
+
+					if (alias_value != NULL)
+						printf("%s=%s\n", alias_name, alias_value);
+					else
+						printf("Alias '%s' not found.\n", alias_name);
+				}
+				i++;
+			}
+		}
 	}
 	else if (_strcmp(args[0], "unalias") == 0)
 		exec_alias_helper(args, alias_count, aliases);
